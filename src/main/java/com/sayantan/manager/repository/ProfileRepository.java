53c0985cd7d52ab0308a3f82f7d0370e5d40bf59
sayantanbar <sayantan22.main@gmail.com>
2025-12-25 15:59:21 +0530

    implement registration of profile

---
+++ src/main/java/com/sayantan/manager/repository/ProfileRepository.java
@@ -0,0 +1,11 @@
+package com.sayantan.manager.repository;
+
+
+import com.sayantan.manager.entity.ProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
+    Optional<ProfileEntity> findByEmail(String email);
+}
